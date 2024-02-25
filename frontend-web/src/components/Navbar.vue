d91b662cf9071cf5648b0986140ab6fb692e89cd
Andrew Kim <kskkim@ucdavis.edu>
2024-02-24 20:19:22 -0800

    initial setup of main page

---
+++ frontend-web/src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="/">
+        <text class="fw-bold" style="color: #000000; font-weight: 600;">GGANBU</text>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+      </div>  
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.navbar.navbar-light{
+  background-color: white!important;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,.2);
+  height: 60px;
+}  
+.container {
+  margin-left:5px;
+}
+</style>
