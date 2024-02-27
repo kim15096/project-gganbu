9942f74731674ad03918522d5e133c1239c47028
Andrew Kim <kim15096@gmail.com>
2024-02-26 22:18:44 -0800

    initial setup of backend

--- backend/gganbu-api.py
+++ backend/gganbu-api.py
@@ -3,17 +3,51 @@ from supabase import create_client, Client
 from datetime import datetime
 from flask_cors import CORS
 import os
+from dotenv import load_dotenv
 
+#################################### SETUP ######################################
 app = Flask(__name__)
 CORS(app)
+load_dotenv()
+SUPABASE_PROJECT_URL = os.getenv('SUPABASE_PROJECT_URL')
+SUPABASE_API_KEY = os.getenv('SUPABASE_API_KEY')
+supabase_client = create_client(SUPABASE_PROJECT_URL, SUPABASE_API_KEY)
 
+#################################### API ENDPOINTS ######################################
 
 '''
 INDEX ROUTE
+List ALL Parameters/Arguments & Return Types
 '''
 @app.route('/')
 def index():
     return "Hello"
+
+
+'''
+Example Route:
+
+@app.route('/auto_complete', methods=['GET', 'POST'])
+def auto_complete():
+    data = request.json
+    
+    category = data['suggested_category']
+    item = data['item']
+    keywords = data['keywords']
+    
+    description = auto_completion.get_completion(item, category, keywords)
+    
+    return description
+'''
+
+#################################### USER ROUTES ######################################
+
+
+
+
+#################################### POST ROUTES ######################################
+
+
 
 
 # DEV: Run on PORT: 8080 
